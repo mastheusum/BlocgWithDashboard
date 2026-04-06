52e701e18f72fb50d822834a0c35dc37792e0176
Victor Matheus <victor.matheus.xd@gmail.com>
2026-04-06 13:38:36 -0300

    Implement user creation and update endpoints
    Testing and review are necessary.

--- server/controllers/user_controller.rb
+++ server/controllers/user_controller.rb
@@ -1,14 +1,44 @@
 require 'sinatra'
 require 'json'
 
+# revisar depois
 class UserController < Sinatra::Base
   get '/user' do
     { message: 'THis route is only a test in UserController' }.to_json
   end
 
   post '/user' do
+    data = JSON.parse request.body.read
+    user = User.new(data)
+    if user.save!
+      status 201
+      { user: user.username, message: 'Usuário cadastrado com sucesso!' }.to_json
+    else
+      status 422
+      { message: 'Dados inválidos' }.to_json
+    end
   end
 
   put '/user/:id' do
+    id = params['id']
+    data = JSON.parse request.body.read
+    token = data[:auth_token]
+    
+    data.delete(:auth_token)
+    
+    session = Session.where(user_id: id)
+    if session.auth_token == token
+      user = User.find(id)
+      if user.update(data)
+        status 202
+        { user: user.username, auth_token: token,message: 'Dados atualizados com sucesso!' }.to_json
+      else
+        status 422
+        { message: 'Dados inválidos' }.to_json
+      end
+    else
+      status 404
+      { message: 'Acesso não autorizado' }.to_json
+    end
   end
 end
